3008ca83c7ad9a5be7b54c1aee13be0ed863ddbc
donghoony <aru0504@naver.com>
2023-08-04 02:31:16 +0900

    feat: add getter in response

--- backend/src/main/java/me/donghoony/cicd/dto/MainResponse.java
+++ backend/src/main/java/me/donghoony/cicd/dto/MainResponse.java
@@ -4,6 +4,9 @@ public class MainResponse {
     public MainResponse(String message) {
         this.message = message;
     }
+    public String getMessage() {
+        return message;
+    }
 
     private String message;
 }
